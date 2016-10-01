dda4878de91dafbcaa43f46d6b4e116b87fcb556
Terry Jia <terry.jia@liferay.com>
2016-10-01 22:36:49 +0800

    move the api changes tests to osgi way

---
+++ com.liferay.blade.test/src/com/liferay/blade/test/apichanges/ContactNameExceptionImportTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.liferay.blade.test.apichanges;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.liferay.blade.api.FileMigrator;
+import com.liferay.blade.api.Problem;
+import com.liferay.blade.upgrade.liferay70.apichanges.ContactNameExceptionImport;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Test;
+import org.osgi.framework.ServiceReference;
+
+public class ContactNameExceptionImportTest extends BaseAPIChangesTest {
+
+	final File testFile = new File("projects/filetests/ContactNameExceptionImport.java");
+
+	@Test
+	public void assetDLgetEntriesTest() throws Exception {
+		List<Problem> problems = null;
+
+		for (ServiceReference<FileMigrator> fm : fileMigrators) {
+			final FileMigrator fmigrator = context.getService(fm);
+
+			if (fmigrator instanceof ContactNameExceptionImport) {
+				problems = fmigrator.analyze(testFile);
+			}
+
+			context.ungetService(fm);
+		}
+
+		assertNotNull(problems);
+		assertEquals(3, problems.size());
+	}
+
+	@Test
+	public void assetDLgetEntriesTestTwice() throws Exception {
+		List<Problem> problems = null;
+
+		for (ServiceReference<FileMigrator> fm : fileMigrators) {
+			final FileMigrator fmigrator = context.getService(fm);
+
+			if (fmigrator instanceof ContactNameExceptionImport) {
+				problems = fmigrator.analyze(testFile);
+
+				problems = fmigrator.analyze(testFile);
+			}
+
+			context.ungetService(fm);
+		}
+
+		assertNotNull(problems);
+		assertEquals(3, problems.size());
+	}
+
+}
